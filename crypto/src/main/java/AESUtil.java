70b080f07b642a51a69d4d45d4e5f778a588ec18
cznno <smeraldo.ash@gmail.com>
2018-06-12 13:58:04 +0800

    update aes

--- crypto/src/main/java/AESUtil.java
+++ crypto/src/main/java/AESUtil.java
@@ -5,7 +5,10 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.security.*;
+import java.util.Base64;
 
 /**
  * AES工具
@@ -14,34 +17,79 @@ import java.security.*;
 public class AESUtil {
 
     private Cipher cipher;
+    private Charset charSet;
+
+    public AESUtil() {
+    }
+
+    public AESUtil(Cipher cipher) {
+        this.cipher = cipher;
+    }
 
     public AESUtil(String cipher, String provider) throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException {
         Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
         this.cipher = Cipher.getInstance(cipher, provider);
+        this.charSet = StandardCharsets.UTF_8;
+    }
+
+    public Cipher getCipher() {
+        return cipher;
+    }
+
+    public void setCipher(Cipher cipher) {
+        this.cipher = cipher;
+    }
+
+    public Charset getCharSet() {
+        return charSet;
+    }
+
+    public void setCharSet(Charset charSet) {
+        this.charSet = charSet;
     }
 
     /**
-     * @param cipherText    密文
+     * @param inputText     密文
      * @param encryptionKey 密钥
-     * @param IV            偏移量
+     * @param iv            偏移量
      * @return 解密文本
      */
-    public String decrypt(byte[] cipherText, String encryptionKey, String IV) throws UnsupportedEncodingException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
-        this.cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF-8")));
-        return new String(this.cipher.doFinal(cipherText), "UTF-8");
+    @Deprecated
+    public byte[] decrypt(String inputText, String encryptionKey, String iv) throws UnsupportedEncodingException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        final String charSetName = this.charSet.name();
+        return decrypt(inputText.getBytes(charSetName), encryptionKey.getBytes(charSetName), iv.getBytes(charSetName));
+    }
+
+    public byte[] decrypt(byte[] input, byte[] key, byte[] iv) throws InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        this.cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key, "AES"), new IvParameterSpec(iv));
+        return this.cipher.doFinal(input);
     }
 
     /**
-     * @param plainText     待加密文本
-     * @param encryptionKey 密钥
-     * @param IV            偏移量
+     * @param inputText 待加密文本
+     * @param key       密钥
+     * @param iv        偏移量
      * @return 密文
      */
-    public byte[] encrypt(String plainText, String encryptionKey, String IV)
+    public byte[] encrypt(String inputText, String key, String iv)
             throws UnsupportedEncodingException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
-        this.cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF-8")));
-        return this.cipher.doFinal(plainText.getBytes("UTF-8"));
+        final String charSetName = this.charSet.name();
+        return encrypt(inputText.getBytes(charSetName), key.getBytes(charSetName), iv.getBytes(charSetName));
+    }
+
+    public byte[] encrypt(byte[] input, byte[] key, byte[] iv) throws InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        this.cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key, "AES"), new IvParameterSpec(iv));
+        return this.cipher.doFinal(input);
+    }
+
+    private byte[] hexStringToBytes(String s) {
+        if (s.length() % 2 > 0) {
+            s = "0" + s;
+        }
+        byte[] bytes = new byte[s.length() / 2];
+        for (int i = 0; i < (s.length() / 2); i++) {
+            bytes[i] = Byte.valueOf(s.substring(i, i + 2));
+        }
+        return bytes;
     }
 }
--- crypto/src/test/java/AESUtilTest.java
+++ crypto/src/test/java/AESUtilTest.java
@@ -21,20 +21,41 @@ class AESUtilTest {
     private AESUtil aesUtil = new AESUtil("AES/CBC/PKCS5Padding", "SunJCE");
     private String origin = "其实我并不理解他在说什么";
     private String encrypted = "sGC7Kb/05i0J+9fRI4NscA0rShxXXBHd92PV46UUyrzmNCEkezJpqTP+5492KNBn";
+    private byte[] encryptedHex = {};
     private String key = "dgafa33sdfar21gh";
     private String iv = "fawrasdfa33swr12";
 
     AESUtilTest() throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException {}
 
     @Test
-    void decrypt() throws IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, UnsupportedEncodingException {
-        final String output = aesUtil.decrypt(Base64.getDecoder().decode(encrypted), key, iv);
-        assertEquals(origin, output);
+    void foo() {
+        String s = "b060bb29bff4e62d09fbd7d123836c700d2b4a1c575c11ddf763d5e3a514cabce63421247b3269a933fee78f7628d067";
+
+//        for (int i = 0; i < s.length(); i += 2) {
+//            System.out.print(s.substring(i, i + 2) + " ");
+//        }
+//        System.out.println();
+//        final byte[] decode = Base64.getDecoder().decode("sGC7Kb/05i0J+9fRI4NscA0rShxXXBHd92PV46UUyrzmNCEkezJpqTP+5492KNBn");
+//        System.out.println(javax.xml.bind.DatatypeConverter.printHexBinary(decode));
     }
 
     @Test
-    void encrypt() throws IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, UnsupportedEncodingException {
+    void singleDecrypt() throws IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, UnsupportedEncodingException {
+        final byte[] output = aesUtil.decrypt(encrypted, key, iv);
+        assertEquals(origin, new String(output, aesUtil.getCharSet()));
+    }
+
+    @Test
+    void singleEncrypt() throws IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, UnsupportedEncodingException {
         final byte[] output = Base64.getEncoder().encode(aesUtil.encrypt(origin, key, iv));
         assertEquals(encrypted, new String(output));
+    }
+
+    @Test
+    void encryptThenDecrypt() throws IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, UnsupportedEncodingException {
+        final String input = "Seeing is believing";
+        final byte[] encrypt = aesUtil.encrypt(input, key, iv);
+        final byte[] decrypt = aesUtil.decrypt(new String(encrypt, aesUtil.getCharSet()), key, iv);
+        assertEquals(input, new String(decrypt, aesUtil.getCharSet()));
     }
 }
